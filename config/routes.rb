2af0a5655a1d4b6b38073e0c0aa8e0581ded5a3a
EC2 Default User <ec2-user@ip-172-31-21-238.us-east-2.compute.internal>
2020-03-27 15:28:31 +0000

    update application

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   
   get "/signup" ,to: "users#new"
   
+  post "/signup" ,to: "users#create"
+  
   get "/login", to: "sessions#new"
   
   post "/login", to: "sessions#create"
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,14 +5,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post signup_path, params: { user: { name:  "",
                                          email: "user@invalid",
-                                         password:              "foo",
+                                         password: "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div#error_explanation'
     assert_select 'div.<CSS class for field with error>'
+    assert_select 'div.field_with_errors'
+    assert_select 'ul' do
+    assert_select 'li' , 'Name can\'t be blank'
+    assert_select 'li' , 'Email is invalid'
+    assert_select 'li' , 'Password confirmation doesn\'t match Password'
+    assert_select 'li' , 'Password is too short (minimum is 6 characters)'
+  end
+    assert_select 'form[action="/signup"]'
   end
 
 
@@ -21,7 +29,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
-                                         password:              "password",
+                                         password: "password",
                                          password_confirmation: "password" } }
     end
     follow_redirect!

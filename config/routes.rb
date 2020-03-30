5075e4624653e129a9dbea6042dfbc9ba2e4a402
EC2 Default User <ec2-user@ip-172-31-21-238.us-east-2.compute.internal>
2020-03-30 16:18:27 +0000

    update application

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,5 @@ Rails.application.routes.draw do
   # resources :posts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  resources :account_activations, only: [:edit]
 end

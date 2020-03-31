3e3843a8b359c2c179d80a7af33ebd13f2671ab2
EC2 Default User <ec2-user@ip-172-31-21-238.us-east-2.compute.internal>
2020-03-31 15:15:33 +0000

    update application

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,14 @@
-class AccountActivationsController < ApplicationController
-end
+def edit 
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+        user.update_attribute(:activated, true)
+        user.update_attribute(:activated_at, Time.zone.now)
+        log_in(user)
+        flash[:success] = "アカウントが有効化されました"
+        redirect_to user
+    else
+        flash[:danger] = "アカウントが無効です"
+        redirect_to root_url
+        
+    end 
+end 

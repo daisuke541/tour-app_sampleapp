9a34349fbb9d13ebee5aa067dd459609030b3ef2
daisuke541 <sirius0509@gmail.com>
2020-04-07 02:42:32 +0000

    update application

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'rails-tutorial-mhartl.c9users.io'
+  host = 'https://2f135bc8f9134f4a8438a3ddbaee1c26.vfs.cloud9.us-east-1.amazonaws.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false

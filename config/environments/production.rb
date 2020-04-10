09dda94ca65d3e29613b87074257c140bdd64598
daisuke541 <sirius0509@gmail.com>
2020-04-10 05:56:52 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,14 +86,14 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'http://toy-appdaisuke.herokuapp.com'
+  host = 'toy-appdaisuke.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => daisuke541,
-    :password       => sirius0509,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }

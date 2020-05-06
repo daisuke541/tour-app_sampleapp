1705944be39cb60f35f728fdb6c0f337e5e98eec
daisuke541 <sirius0509@gmail.com>
2020-05-06 10:48:42 +0000

    update

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -4,6 +4,7 @@ class GuestSessionsController < ApplicationController
     def create
         user = User.find_or_create_by(name: "ゲストユーザー", email: 'test@example.com', 
         password_digest: SecureRandom.urlsafe_base64)
+        @feed_items = @user.feed.paginate(page: params[:page])
         session[:user_id] = user.id
         flash[:success] = 'ゲストユーザーでログインしました'
         flash[:warning] = 'よろしくお願いします！'
